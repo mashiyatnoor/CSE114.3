8024ba2f1f01456a97f7c5b107b21d451a96c78c
mashiyatnoor <261006712@eastdelta.edu.bd>
2026-06-25 20:22:29 +0600

    Create codeforces.c

---
+++ Lab 4/codeforces.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int w;
+    scanf("%d" ,&w);
+    if(w%2==0 && w>=4){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+ 
+    return 0;
+}
